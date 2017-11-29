feca1abfbd449178d05fada77d47830ff2198194
Kasp5953 <kasp5953@edu.easj.dk>
2017-11-29 11:58:53 +0100

    fetch methods now async

--- Kode/TouchPoint/TouchPoint/Database/FetchFromDB.cs
+++ Kode/TouchPoint/TouchPoint/Database/FetchFromDB.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace TouchPoint.Database {
     public static class FetchFromDB<T> where T : ISaveable {
-        public static T FetchSingle(string table, int id) {
+        public static Task<T> FetchSingle(string table, int id) {
             throw new NotImplementedException();
         }
 
-        public static List<T> FetchMultiple(string table) {
+        public static List<Task<T>> FetchMultiple(string table) {
             throw new NotImplementedException();
         }
     }
