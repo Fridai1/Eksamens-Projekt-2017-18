ca42f10648157db93443144b6a8462084d1870ad
Kasp5953 <kasp5953@edu.easj.dk>
2017-12-06 13:07:09 +0100

    serialising objects

--- Kode/TouchPoint/TouchPoint/Database/SaveToDB.cs
+++ Kode/TouchPoint/TouchPoint/Database/SaveToDB.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace TouchPoint.Database {
     public class SaveToDB<T> where T : ISaveable {
@@ -15,15 +16,20 @@ namespace TouchPoint.Database {
             _handler.UseDefaultCredentials = true;
         }
         
-
+        
         public async Task Save(T item) {
             using(var client = new HttpClient(_handler)) {
                 client.BaseAddress = new Uri(_GLOBALS.ServerUrl);
                 client.DefaultRequestHeaders.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                if(item.Id == -1) {
-                    await client.PostAsync(client.BaseAddress, new StringContent(item.PropertyFormat()));
+                string serialised = JsonConvert.SerializeObject(item);
+                StringContent content = new StringContent(serialised, Encoding.UTF8, " ");
+
+                if (item.Id == -1) {
+                    await client.PostAsync(client.BaseAddress, content);
+                } else {
+                    await client.PutAsync(client.BaseAddress, content);
                 }
             }
 
