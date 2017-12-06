22128127a272ca171ccf5eab0d68e7b03bb62380
Fridai <Nikolaiviby3@gmail.com>
2017-12-06 15:00:31 +0100

    login i undervisningssted

--- Kode/TouchPoint/TouchPoint/ViewModel/Domain/Undervisningssted/USMasterDetailsVm.cs
+++ Kode/TouchPoint/TouchPoint/ViewModel/Domain/Undervisningssted/USMasterDetailsVm.cs
@@ -4,21 +4,13 @@ namespace TouchPoint.ViewModel.Undervisningssted
 {
     public class USMasterDetailsVm : MasterDetailsVMBase<TouchPoint.Undervisningssted>
     {
-        private RelayCommand _lokaleCommand;
-        private string _lokale;
+        
         public USMasterDetailsVm() : base(new USFactoryVm())
         {
-            _lokaleCommand = new RelayCommand(AddLokale,()=>true);
         }
 
-        public string Lokale
-        {
-            set => _lokale = value;
-        }
+        
 
-        public void AddLokale()
-        {
-            
-        }
+       
     }
 }
--- Kode/TouchPoint/TouchPoint/ViewModel/Domain/Undervisningssted/USOverviewVm.cs
+++ Kode/TouchPoint/TouchPoint/ViewModel/Domain/Undervisningssted/USOverviewVm.cs
@@ -1,17 +1,26 @@
-﻿using TouchPoint.ViewModel.Undervisningssted.Lokale;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Command.Implementation;
+using TouchPoint.Annotations;
+using TouchPoint.ViewModel.Undervisningssted.Lokale;
 
 namespace TouchPoint.ViewModel.Undervisningssted
 {
-    public class USOverviewVm
+    public class USOverviewVm : INotifyPropertyChanged
     {
         private USMasterDetailsVm _masterDetails;
         private LokaleMasterDetailsVm _lokalerMasterDetails;
         private Bruger _loggedInUser;
+        private RelayCommand _tilføjLokaleCommand;
         public USOverviewVm()
         {
             _masterDetails = new USMasterDetailsVm();
             _lokalerMasterDetails = new LokaleMasterDetailsVm();
             _loggedInUser = LoginVm.LoggedInUser;
+            _tilføjLokaleCommand = new RelayCommand(Opretlokale, ()=>true);
+            
+            
 
         }
 
@@ -25,9 +34,29 @@ namespace TouchPoint.ViewModel.Undervisningssted
             get => _lokalerMasterDetails;
         }
 
-        public Bruger GetLoggedInUSer
+        public string GetLoggedInUSer
         {
-            get => _loggedInUser;
+            get => _loggedInUser.Navn;
+        }
+
+        private void Opretlokale()
+        {
+            LokaleMasterDetails.Create();
+            MasterDetails.DetailsVM.DomainObject.OpretLokale(LokaleMasterDetails.DetailsVM.DomainObject.Navn);
+        }
+
+        public ICommand TilføjLokale
+        {
+            get => _tilføjLokaleCommand;
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
