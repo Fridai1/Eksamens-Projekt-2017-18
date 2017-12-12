bef09f687547b1690d4c466fd99c6ce6223927f8
SabreUrn <kasp5953@edu.easj.dk>
2017-12-12 11:32:54 +0100

    validation basic framework

---
+++ Kode/TouchPoint/TouchPoint/Validation/ValidationHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchPoint.Validation {
+    public class ValidationHandler {
+        public static ValidationOutput Validate<TValue>(TValue value, Func<TValue, bool> isValid, string message) {
+            return (isValid(value) ? null : new ValidationOutput(message)); //returns null if Func bool checks out, otherwise returns `message`
+        }
+
+        //gets called from domain-specific Validation classes
+        public static void Throw<TValue>(Func<TValue, ValidationOutput> validator, TValue value) {
+            ValidationOutput vo = validator(value);
+            if(vo != null) {
+                throw new ArgumentException(vo.Message);
+            }
+        }
+
+        public static ValidationOutput ValidateExampleHelloWorldLength(string value, int length,    //string to validate, length that string must be.
+        [CallerMemberName] string propertyName = null) {                                            //CallerMemberName takes name of member which called this method.
+            string message = propertyName + " must contain " + " characters";                       //the error message sent to Validate.
+            return Validate<string>(value, (v => v.Length == length), message);                     //returns message one line above if (v.Length != length),
+                                                                                                    //otherwise null (see Validate)
+        }
+    }
+}
---
+++ Kode/TouchPoint/TouchPoint/Validation/ValidationOutput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchPoint.Validation {
+    public class ValidationOutput {
+        public string Message {
+            get;
+        }
+
+        public ValidationOutput(string message) {
+            Message = message;
+        }
+    }
+}
