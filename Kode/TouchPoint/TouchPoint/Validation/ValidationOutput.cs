e0732fe49ee33d320d40520b6336aad3dbb76877
SabreUrn <kasp5953@edu.easj.dk>
2017-12-18 07:53:21 +0100

    basic validation framework

---
+++ Kode/TouchPoint/TouchPoint/Validation/ValidationOutput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchPoint.Validation {
+    public class ValidationOutput {
+        private string _message;
+
+        public string Message {
+            get { return _message; }
+        }
+
+        public ValidationOutput(string message) {
+            _message = message;
+        }
+    }
+}
